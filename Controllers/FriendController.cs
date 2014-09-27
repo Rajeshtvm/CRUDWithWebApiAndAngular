379de863820b2797ddde4a521084961db587b926
Rajeshtvm <rajeshstvm@gmail.com>
2014-09-26 22:22:18 -0400

    CRUD operation with Web Api and Angular
    Experiment in Web Api and Angular

---
+++ Controllers/FriendController.cs
@@ -0,0 +1,91 @@
+﻿using CRUDWithWebApiAndAngular.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CRUDWithWebApiAndAngular.Controllers
+{
+    public class FriendController : ApiController
+    {
+        private FriendsContext db = new FriendsContext();
+
+        [HttpGet]
+        public IEnumerable<Friend> Get()
+        {
+            return db.Friends.AsEnumerable();
+        }
+
+        public Friend Get(int id)
+        {
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+
+            return friend;
+        }
+
+        public HttpResponseMessage Post(Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Friends.Add(friend);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, friend);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = friend.FriendId }));
+                return response;
+            }
+            else
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        }
+
+        public HttpResponseMessage Put(int id, Friend friend)
+        {
+            if (!ModelState.IsValid)
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+
+            if (id != friend.FriendId)
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+
+            db.Entry(friend).State = System.Data.EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            db.Friends.Remove(friend);
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, friend);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Scripts/app.js
@@ -0,0 +1,69 @@
+﻿function friendController($scope, $http) {
+    $scope.loading = true;
+    $scope.addMode = false;
+
+    $http.get('api/Friend/').success(function (data) {
+        $scope.friends = data;
+        $scope.loading = false;
+    }).error(function () {
+        $scope.error = "An error has occured while loadin post!";
+        $scope.loading = false;
+    });
+
+    $scope.toggleEdit = function () {
+        this.friend.editMode = !this.friend.editMode;
+    };
+    $scope.toggleAdd = function () {
+        $scope.addMode = !this.friend.addMode;
+    };
+
+    //Upadte/Edit Records
+    $scope.save = function () {
+        alert("Edit");
+        $scope.loading = true;
+        var frien = this.friend;
+        $http.put('api/Friend/', frien).success(function (data) {
+            alert("Updated Successfully!!");
+            $scope.loading = false;
+            }).error(function(data){
+                $scope.error = "An error has occured while updating friends!!" + data;
+                $scope.loading = false;
+            });
+
+    };
+
+    //Add new records
+
+    $scope.add = function(){
+        $scope.loading = true;
+        $http.post('api/Friend', this.newFriend).success(function (data) {
+            alert("Added successfully!!");
+            $scope.addMode = false;
+            $scope.friends.push(data);
+            $scope.loading = false;
+        }).error(function (data) {
+            $scope.error = "An Error has occured while Adding Friend! " + data;
+            $scope.loading = false;
+        });
+
+    };
+    $scope.deletefriend = function () {
+        $scope.loading = true;
+        var friendid = this.friend.FriendId;
+        $http.delete('/api/Friend/' + friendid).success(function (data) {
+            alert("Deleted Successfully!!");
+            $.each($scope.friends, function (i) {
+                if ($scope.friends[i].FriendId === friendid) {
+                    $scope.friends.splice(i, 1);
+                    return false;
+                }
+            });
+            $scope.loading = false;
+        }).error(function (data) {
+            $scope.error = "An Error has occured while Saving Friend! " + data;
+            $scope.loading = false;
+
+        });
+    };
+
+}
